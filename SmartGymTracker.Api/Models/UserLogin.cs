68c8972a7558a613f608db733a58f7d27b079b34
Nicholas Holguin <nch22a@fsu.edu>
2025-09-21 21:22:42 -0400

    added 2 classes to the model

---
+++ SmartGymTracker.Api/Models/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartGymTracker.Api.Models
+{
+    public class UserLogin
+    {
+        public string? UserId { get; set; } = default!;
+        public string? username { get; set; } 
+        public string? password { get; set; }
+        public string? firstname { get; set; }
+        public string? lastname { get; set; }
+        public string? email { get; set; }
+        public string? phonenumber { get; set; }
+        public string? dateofbirth { get; set; }
+        public int weight { get; set; }
+        public int height { get; set; }
+        public string? gender {get; set; }
+    }
+}
