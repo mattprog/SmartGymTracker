cb4e34db04d8b297063ca2c0fc0241c9aa5517eb
amanda orama <ao22h@fsu.edu>
2025-10-14 12:27:33 -0400

    Added User Management page, login & registration updates

--- frontend/smart-gym-tracker/src/App.js
+++ frontend/smart-gym-tracker/src/App.js
@@ -8,6 +8,8 @@ import Progress from './pages/Progress';
 import Admin from './pages/Admin';
 import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
+import UserManagement from './pages/UserManagement';
+
 
 
 
@@ -46,6 +48,7 @@ function App() {
         {currentPage === 'admin' && <Admin/>}
         {currentPage === 'registration' && <RegistrationPage />}
         {currentPage === 'login' && <LoginPage setCurrentPage={setCurrentPage} />}
+        {currentPage === 'usermanagement' && <UserManagement />}
       </div>
 
 
@@ -99,6 +102,14 @@ function App() {
         <span className="text-sm">Login</span>
         </div>
 
+
+
+        {/* Temporary User Management Button */}
+        <div className="flex flex-col items-center cursor-pointer" onClick={() => setCurrentPage('usermanagement')}>
+        <div className="w-6 h-6 bg-gray-200 rounded-full mb-1"></div>
+        <span className="text-sm">User Mgmt</span>
+        </div>
+        
         </div>
       )}
     </div>
